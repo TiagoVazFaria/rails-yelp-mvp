611c8b6c593864a12491226ed50779042e2f2c14
Tiago Faria <tiago.vazfaria14@gmail.com>
2019-08-21 15:34:14 +0100

    done

--- Gemfile
+++ Gemfile
@@ -31,7 +31,11 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem 'autoprefixer-rails'
+gem 'font-awesome-sass', '~> 5.6.1'
+gem 'simple_form'
 
+gem 'faker', '~> 2.1.2'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+
+5.times do Restaurant.create(name: Faker::Restaurant.name,
+     address: Faker::Address.city,
+     phone_number: Faker::PhoneNumber.phone_number,
+     category: ['chinese',
+                'italian',
+                'japanese',
+                'french',
+                'belgian'].sample)
+end
+
+Restaurant.all.each do |restaurant|
+  rand(5..10).times do
+      restaurant.reviews << Review.create(content: Faker::Restaurant.review, rating: rand(0..5))
+  end
+end
+
